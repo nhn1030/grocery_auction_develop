6e8c36e9dc0101355838579ff981ba94cf05446a
nhn1030 <shgusska12@gmail.com>
2023-11-29 18:03:26 +0000

    chore: estimate의 static 파일을 찾을 수 있도록 수정

--- grocery_auction/settings.py
+++ grocery_auction/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'market'
+    'market',
+    'estimate',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'estimate/static')
+]
