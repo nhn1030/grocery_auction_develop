c57727403fec55c44b0bacb38bae0964f2d164bf
nhn1030 <shgusska12@gmail.com>
2023-11-30 06:48:19 +0000

    feat: 사이드바 탭이동 구현

--- estimate/static/js/sidebar.js
+++ estimate/static/js/sidebar.js
@@ -1,43 +1,43 @@
 // sidebar.js
 
 // 각 탭에 클릭 이벤트 리스너 추가
-var homeTab = document.getElementById("home-tab");
-var dashboardTab = document.getElementById("dashboard-tab");
-var ordersTab = document.getElementById("orders-tab");
-var productsTab = document.getElementById("products-tab");
-var customersTab = document.getElementById("customers-tab");
+var productsTab = document.getElementById("selectProductsTab");
+var dayAndBulkTab = document.getElementById("conditionForBulkWithDay");
+var rangeOfBudgetTab = document.getElementById("rangeOfBudget");
+var detailTab = document.getElementById("detail");
+var confirmTab = document.getElementById("finalConfirmation");
 
 function setActiveTab(tab) {
     // 모든 탭의 활성화 클래스 제거
-    homeTab.classList.remove("active");
-    dashboardTab.classList.remove("active");
-    ordersTab.classList.remove("active");
     productsTab.classList.remove("active");
-    customersTab.classList.remove("active");
+    dayAndBulkTab.classList.remove("active");
+    rangeOfBudgetTab.classList.remove("active");
+    detailTab.classList.remove("active");
+    confirmTab.classList.remove("active");
 
     // 선택한 탭에 활성화 클래스 추가
     tab.classList.add("active");
 }
 
-homeTab.addEventListener("click", function() {
-    setActiveTab(homeTab);
-});
-
-dashboardTab.addEventListener("click", function() {
-    setActiveTab(dashboardTab);
-});
-
-ordersTab.addEventListener("click", function() {
-    setActiveTab(ordersTab);
-});
-
 productsTab.addEventListener("click", function() {
     setActiveTab(productsTab);
 });
 
-customersTab.addEventListener("click", function() {
-    setActiveTab(customersTab);
+dayAndBulkTab.addEventListener("click", function() {
+    setActiveTab(dayAndBulkTab);
+});
+
+rangeOfBudgetTab.addEventListener("click", function() {
+    setActiveTab(rangeOfBudgetTab);
+});
+
+detailTab.addEventListener("click", function() {
+    setActiveTab(detailTab);
+});
+
+confirmTab.addEventListener("click", function() {
+    setActiveTab(confirmTab);
 });
 
 // 페이지 로드 시 초기 탭 설정
-setActiveTab(homeTab);
+setActiveTab(productsTab);
